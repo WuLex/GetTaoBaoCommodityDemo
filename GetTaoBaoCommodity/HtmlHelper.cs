739b4128649b8d0e02ce44ae458094360d7e989b
wulex <827937686@qq.com>
2023-06-15 22:47:39 +0800

    modify files

--- GetTaoBaoCommodity/HtmlHelper.cs
+++ GetTaoBaoCommodity/HtmlHelper.cs
@@ -39,10 +39,6 @@ namespace GetTaoBaoCommodity
                 // ignored
             }
 
-
-         
-
-
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < resultList.Count; i++)
             {
@@ -92,80 +88,80 @@ namespace GetTaoBaoCommodity
         }
 
 
-        public static void WriteFile(List<List<string>> resultAarry, string tmppath, string resultpath)
-        {
-            //---------------------读html模板页面到stringbuilder对象里----
-            StringBuilder htmltext = new StringBuilder();
-            if (Directory.Exists(tmppath))//判断是否存在
-            {
-                Directory.CreateDirectory(tmppath);//创建新路径
+        //public static void WriteFile(List<List<string>> resultAarry, string tmppath, string resultpath)
+        //{
+        //    //---------------------读html模板页面到stringbuilder对象里----
+        //    StringBuilder htmltext = new StringBuilder();
+        //    if (Directory.Exists(tmppath))//判断是否存在
+        //    {
+        //        Directory.CreateDirectory(tmppath);//创建新路径
 
-            }
-            try
-            {
-                using (StreamReader sr = new StreamReader(tmppath)) //模板页路径
-                {
-                    String line;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        htmltext.Append(line);
-                    }
+        //    }
+        //    try
+        //    {
+        //        using (StreamReader sr = new StreamReader(tmppath)) //模板页路径
+        //        {
+        //            String line;
+        //            while ((line = sr.ReadLine()) != null)
+        //            {
+        //                htmltext.Append(line);
+        //            }
 
-                    sr.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                // ignored
-            }
+        //            sr.Close();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex);
+        //        // ignored
+        //    }
 
-            StringBuilder sb = new StringBuilder();
-            for (int x = 0; x < resultAarry.Count; x++)
-            {
-                for (int y = 0; y < resultAarry[x].Count; y++)
-                {
-                    if (y == 1)
-                    {
-                        sb.Append(
-                            $"<tr><td width=\"100%\" valign=\"middle\" align=\"left\">第{x + 1}个-{Rule.Information[y][1]}-<img src='{"http:" + resultAarry[x][y]}' width='128' height='128' /></td></tr>\r\n");
-                    }
-                    else if (y == 2)
-                    {
-                        sb.Append(String.Format(
-                            "<tr><td width=\"100%\" valign=\"middle\" align=\"left\">第{0}个-<a href='{2}'>{1}</a></td></tr>\r\n",
-                            x + 1, Rule.Information[y][1], "https:" + resultAarry[x][y]));
-                    }
-                    else
-                    {
-                        sb.Append(
-                            $"<tr><td width=\"100%\" valign=\"middle\" align=\"left\">第{x + 1}个-{Rule.Information[y][1]}-{resultAarry[x][y]}</td></tr>\r\n");
-                    }
-                }
+        //    StringBuilder sb = new StringBuilder();
+        //    for (int x = 0; x < resultAarry.Count; x++)
+        //    {
+        //        for (int y = 0; y < resultAarry[x].Count; y++)
+        //        {
+        //            if (y == 1)
+        //            {
+        //                sb.Append(
+        //                    $"<tr><td width=\"100%\" valign=\"middle\" align=\"left\">第{x + 1}个-{Rule.Information[y][1]}-<img src='{"http:" + resultAarry[x][y]}' width='128' height='128' /></td></tr>\r\n");
+        //            }
+        //            else if (y == 2)
+        //            {
+        //                sb.Append(String.Format(
+        //                    "<tr><td width=\"100%\" valign=\"middle\" align=\"left\">第{0}个-<a href='{2}'>{1}</a></td></tr>\r\n",
+        //                    x + 1, Rule.Information[y][1], "https:" + resultAarry[x][y]));
+        //            }
+        //            else
+        //            {
+        //                sb.Append(
+        //                    $"<tr><td width=\"100%\" valign=\"middle\" align=\"left\">第{x + 1}个-{Rule.Information[y][1]}-{resultAarry[x][y]}</td></tr>\r\n");
+        //            }
+        //        }
 
-                sb.Append(String.Format(
-                    "<tr><td width=\"100%\" bgcolor='#30D5C8' valign=\"middle\" align=\"left\"></td></tr>\r\n"));
-            }
+        //        sb.Append(String.Format(
+        //            "<tr><td width=\"100%\" bgcolor='#30D5C8' valign=\"middle\" align=\"left\"></td></tr>\r\n"));
+        //    }
 
-            //----------替换htm里的标记为你想加的内容
-            htmltext.Replace("$htmlformat[1]", sb.ToString());
+        //    //----------替换htm里的标记为你想加的内容
+        //    htmltext.Replace("$htmlformat[1]", sb.ToString());
 
-            //----------生成htm文件------------------――
-            try
-            {
-                using (StreamWriter sw = new StreamWriter(resultpath, false, System.Text.Encoding.GetEncoding("utf-8"))) //保存地址
-                {
-                    sw.WriteLine(htmltext);
-                    sw.Flush();
-                    sw.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                // ignored
-            }
-        }
+        //    //----------生成htm文件------------------――
+        //    try
+        //    {
+        //        using (StreamWriter sw = new StreamWriter(resultpath, false, System.Text.Encoding.GetEncoding("utf-8"))) //保存地址
+        //        {
+        //            sw.WriteLine(htmltext);
+        //            sw.Flush();
+        //            sw.Close();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex);
+        //        // ignored
+        //    }
+        //}
 
         /// <summary>
         /// 自动产生编号，最多一天产生99个
--- GetTaoBaoCommodity/Models/Rule.cs
+++ GetTaoBaoCommodity/Models/Rule.cs
@@ -4,41 +4,97 @@ using System.Text.RegularExpressions;
 
 namespace GetTaoBaoCommodity.Models
 {
+    /// <summary>
+    /// 重构前的代码
+    /// </summary>
+    //public static class Rule
+    //{
+    //    public static string[] Name = { "raw_title", "商品名称" };
+    //    public static string[] Pic = { "pic_url", "图片地址" };
+    //    public static string[] Url = { "detail_url", "商品地址" };
+    //    public static string[] Price = { "view_price", "价格" };
+    //    public static string[] Address = { "item_loc", "店铺所在地" };
+    //    public static string[] BuyNumber = { "view_sales", "购买人数" };
+    //    public static string[] UserId = { "user_id", "卖家ID" };
+    //    public static string[] ShopName = { "nick", "店铺名称" };
+    //    public static int Inline = 58; //内容所在行数
+
+    //    public static List<string[]> Information = new List<string[]>() {Name, Pic, Url, Price, Address, BuyNumber, UserId, ShopName};
+
+    //    public static List<List<string>> GetAll(string html) //获取所有商品信息
+    //    {
+    //        List<List<string>> list = new List<List<string>>();
+    //        for (int i = 1; i <= 10; i++)
+    //        {
+    //            List<string> item = new List<string>();
+    //            for (int j = 0; j < Information.Count; j++)
+    //            {
+    //                int index = html.IndexOf(Information[j][0], StringComparison.Ordinal);
+    //                if (index != -1)
+    //                {
+    //                    html = html.Substring(index + Information[j][0].Length + 3);
+    //                    item.Add(j == 2
+    //                        ? Regex.Unescape(html.Substring(0, html.IndexOf('"')))
+    //                        : html.Substring(0, html.IndexOf('"')));
+    //                }
+    //            }
+
+    //            list.Add(item);
+    //        }
+
+    //        return list;
+    //    }
+    //}
+
     public static class Rule
     {
-        public static string[] Name = { "raw_title", "商品名称" };
-        public static string[] Pic = { "pic_url", "图片地址" };
-        public static string[] Url = { "detail_url", "商品地址" };
-        public static string[] Price = { "view_price", "价格" };
-        public static string[] Address = { "item_loc", "店铺所在地" };
-        public static string[] BuyNumber = { "view_sales", "购买人数" };
-        public static string[] UserId = { "user_id", "卖家ID" };
-        public static string[] ShopName = { "nick", "店铺名称" };
+        public static (string key, string label) Name = ("raw_title", "商品名称");
+        public static (string key, string label) Pic = ("pic_url", "图片地址");
+        public static (string key, string label) Url = ("detail_url", "商品地址");
+        public static (string key, string label) Price = ("view_price", "价格");
+        public static (string key, string label) Address = ("item_loc", "店铺所在地");
+        public static (string key, string label) BuyNumber = ("view_sales", "购买人数");
+        public static (string key, string label) UserId = ("user_id", "卖家ID");
+        public static (string key, string label) ShopName = ("nick", "店铺名称");
         public static int Inline = 58; //内容所在行数
 
-        public static List<string[]> Information = new List<string[]>() {Name, Pic, Url, Price, Address, BuyNumber, UserId, ShopName};
-
-        public static List<List<string>> GetAll(string html) //获取所有商品信息
+        public static Dictionary<string, (string key, string label)> Information = new Dictionary<string, (string key, string label)>
         {
-            List<List<string>> list = new List<List<string>>();
+            { "Name", Name },
+            { "Pic", Pic },
+            { "Url", Url },
+            { "Price", Price },
+            { "Address", Address },
+            { "BuyNumber", BuyNumber },
+            { "UserId", UserId },
+            { "ShopName", ShopName },
+        };
+
+
+        /// <summary>
+        /// 获取所有商品信息
+        /// </summary>
+        /// <param name="html"></param>
+        /// <returns></returns>
+        public static List<List<string>> GetAll(string html)
+        {
+            var list = new List<List<string>>();
             for (int i = 1; i <= 10; i++)
             {
-                List<string> item = new List<string>();
-                for (int j = 0; j < Information.Count; j++)
+                var item = new List<string>();
+                foreach (var info in Information.Values)
                 {
-                    int index = html.IndexOf(Information[j][0], StringComparison.Ordinal);
+                    int index = html.IndexOf(info.key, StringComparison.Ordinal);
                     if (index != -1)
                     {
-                        html = html.Substring(index + Information[j][0].Length + 3);
-                        item.Add(j == 2
+                        html = html.Substring(index + info.key.Length + 3);
+                        item.Add(info.key == "Url"
                             ? Regex.Unescape(html.Substring(0, html.IndexOf('"')))
                             : html.Substring(0, html.IndexOf('"')));
                     }
                 }
-
                 list.Add(item);
             }
-
             return list;
         }
     }
